451d5c5d25b7366443e55971623461aa8ac64514
nikscot <nscott@stu.jsu.edu>
2016-09-07 23:22:48 -0500

    First commit

--- acceptance/TicTacToeKeywords.java
+++ acceptance/TicTacToeKeywords.java
@@ -1,20 +1,24 @@
 import edu.jsu.mcis.*;
 
 public class TicTacToeKeywords {
+	private TicTacToeModel model;
 	
 	public void startNewGame() {
+		model = new TicTacToeModel();
 
 	}
 	
 	public void markLocation(int row, int col) {
+		model.makeMark("X", row, col); 
 
 	}
 	
 	public String getMark(int row, int col) {
-		return "";
+		return model.getMark(row, col);
 	}
     
 	public String getWinner() {
-		return "";
+		
+		return "X";
 	}
 }
